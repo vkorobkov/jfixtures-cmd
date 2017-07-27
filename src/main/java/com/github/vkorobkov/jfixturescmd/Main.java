08754c7cec6888b531808bcc0482b47768a7b93b
Rodionov Alexander <allrod@mail.ru>
2017-07-27 13:12:56 +0300

    init a beautiful prompt

--- src/main/java/com/github/vkorobkov/jfixturescmd/Main.java
+++ src/main/java/com/github/vkorobkov/jfixturescmd/Main.java
@@ -1,4 +1,16 @@
 package com.github.vkorobkov.jfixturescmd;
 
-public class Main {
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String[] args) throws IOException {
+        final String asciiJFixtures = FigletFont.convertOneLine("JFIXTURES");
+        System.out.println(asciiJFixtures);
+        System.out.println("For more info please visit https://github.com/vkorobkov/jfixtures-cmd");
+    }
 }
