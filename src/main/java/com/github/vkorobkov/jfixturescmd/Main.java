c004d28f1a07a2467a8a6a01533811604d73eaa4
Rodionov Aleksandr <allrod@mail.ru>
2017-07-28 14:21:00 +0300

    logback logger (#12)
    * added logback logger

--- src/main/java/com/github/vkorobkov/jfixturescmd/Main.java
+++ src/main/java/com/github/vkorobkov/jfixturescmd/Main.java
@@ -1,12 +1,10 @@
 package com.github.vkorobkov.jfixturescmd;
 
-import static com.github.vkorobkov.jfixturescmd.Prompt.writePrompt;
-
 public final class Main {
     private Main() {
     }
 
     public static void main(String[] args) {
-        writePrompt(System.out);
+        Prompt.write();
     }
 }
--- src/main/java/com/github/vkorobkov/jfixturescmd/Prompt.java
+++ src/main/java/com/github/vkorobkov/jfixturescmd/Prompt.java
@@ -2,19 +2,17 @@ package com.github.vkorobkov.jfixturescmd;
 
 import com.github.lalyos.jfiglet.FigletFont;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
-import java.io.PrintStream;
-
+@Slf4j
 final class Prompt {
     private Prompt() {
     }
 
     @SneakyThrows
-    static void writePrompt(PrintStream printStream) {
+    static void write() {
         final String asciiJFixtures = FigletFont.convertOneLine("JFIXTURES");
-        printStream
-                .append(asciiJFixtures)
-                .append("For more info please visit https://github.com/vkorobkov/jfixtures-cmd");
-        printStream.println();
+        log.info(asciiJFixtures);
+        log.info("For more info please visit https://github.com/vkorobkov/jfixtures-cmd");
     }
 }
