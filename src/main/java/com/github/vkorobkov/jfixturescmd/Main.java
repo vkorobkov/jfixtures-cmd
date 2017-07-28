2a1dc4d3f31aba2fdf3c293ab4216629b4b36e14
Rodionov Alexander <allrod@mail.ru>
2017-07-28 14:17:52 +0300

    fixed Pr remarks

--- src/main/java/com/github/vkorobkov/jfixturescmd/Main.java
+++ src/main/java/com/github/vkorobkov/jfixturescmd/Main.java
@@ -1,12 +1,10 @@
 package com.github.vkorobkov.jfixturescmd;
 
-import static com.github.vkorobkov.jfixturescmd.Prompt.write;
-
 public final class Main {
     private Main() {
     }
 
     public static void main(String[] args) {
-        write();
+        Prompt.write();
     }
 }
