cdb427607fc14bd5727d1e6ad81dc19791072681
Rodionov Alexander <allrod@mail.ru>
2017-08-03 15:37:20 +0300

    added SqlTypeTest

---
+++ src/test/groovy/com/github/vkorobkov/jfixturescmd/SqlTypeTest.groovy
@@ -0,0 +1,25 @@
+package com.github.vkorobkov.jfixturescmd
+
+import spock.lang.Specification
+
+class SqlTypeTest extends Specification {
+    def "SqlType positive cases"(String value, expected) {
+        expect:
+        SqlType.valueOf(value) == expected
+
+        where:
+        value        | expected
+        "POSTGRES"   | SqlType.POSTGRES
+        "MYSQL"      | SqlType.MYSQL
+        "H2"         | SqlType.H2
+        "CLICKHOUSE" | SqlType.CLICKHOUSE
+    }
+
+    def "SqlType throws exception when wrong value is provided"() {
+        when:
+        SqlType.valueOf("wrong_type")
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+}
