65c33d9523a7d0143e50a990f3583c190cd709e9
Rodionov Alexander <allrod@mail.ru>
2017-08-09 13:10:39 +0300

    refactoring

--- src/test/groovy/com/github/vkorobkov/jfixturescmd/JarIntegrationTest.groovy
+++
@@ -1,8 +0,0 @@
-package com.github.vkorobkov.jfixturescmd
-
-class JarIntegrationTest {
-    static void main(String[] args) {
-        def jarOutput = "java -jar target/jfixtures-cmd-1.0.0.jar --help".execute().text
-        assert jarOutput.contains("Usage: JFixtures CMD [options]")
-    }
-}
