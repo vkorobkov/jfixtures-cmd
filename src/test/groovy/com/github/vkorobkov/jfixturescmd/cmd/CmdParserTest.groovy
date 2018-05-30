2bbaa2bd85b8fc06070ff84f27cd6d9e0312482c
Rodionov Aleksandr <allrod@mail.ru>
2018-05-30 08:26:09 +0300

    Update dependencies

--- src/test/groovy/com/github/vkorobkov/jfixturescmd/cmd/CmdParserTest.groovy
+++ src/test/groovy/com/github/vkorobkov/jfixturescmd/cmd/CmdParserTest.groovy
@@ -85,14 +85,14 @@ class CmdParserTest extends Specification {
         "sql99" | "sql99"
     }
 
-    def "fixtures folder is wrong test"() {
+    def "fixtures folder or file is wrong test"() {
         given:
         String[] args = ["-src", "wrong_path", "-dst", "out.sql", "-type", "mysql"]
         when:
         cmdParser.parse(args)
         then:
         сmdArgs.source == "wrong_path"
-        TestPrintStream.contains("Failed to load fixtures: Can not load fixtures from directory: wrong_path")
+        TestPrintStream.contains("NoSuchFileException: wrong_path")
         TestPrintStream.contains("Usage")
     }
 
